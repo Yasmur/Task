3f509cd07e7196b35e1ebfe5f57cadf036dd66cd
Your Name <your_email@whatever.com>
2021-12-25 23:42:31 +0200

    start task

---
+++ src/App.js
@@ -0,0 +1,139 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+    <div className="nav">
+    <div className="list">
+      <h1>Product List</h1>
+      </div>
+      <div className="button">
+        <button className="add">ADD</button>
+        <button id="delete-product-btn">MASS DELETE</button>
+      </div>
+      </div>
+      <hr></hr>
+    <div className="main">
+    <div className="block">
+      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>
+      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>
+      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>
+      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>      <div className="item">
+        {" "}
+        <input type="checkbox" id="delete-checkbox" name=""></input>{" "}
+        <div className="item-description">
+          <div className="code">JVC200123</div>
+          <div className="name">Acme DISC</div>
+          <div className="price">1.00 $</div>
+          <div className="other">Size: 700MB</div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <hr></hr>
+    <div className="foot">fsfafasfasfasfasfaf</div>
+    </div>
+  );
+}
+
+export default App;
